bef14e2b86a5d75c9e7d7453b0df26b751c6363a
codingsagar <sagarishere123@gmail.com>
2023-02-16 12:38:55 +0530

    react router changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { createBrowserRouter ,createRoutesFromElements, Route,RouterProvider} fr
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root/>}>
+    <Route path="/redux-toolkit-online-shop/" element={<Root/>}>
       <Route path="/" index element={<Products />} />
       <Route path="cart" element={<Cart />} />
     </Route>
