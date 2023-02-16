725266f718ce60d83e0d5263b6daccf5cee1d333
codingsagar <sagarishere123@gmail.com>
2023-02-16 16:30:42 +0530

    fixing the issue

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { createBrowserRouter ,createRoutesFromElements, Route,RouterProvider} fr
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Root/>} errorElement={<h1 className='text-danger'>ERROR</h1>}>
+    <Route path="/" element={<Root/>} errorElement={<Root/>}>
       <Route path="/" index element={<Products />} />
       <Route path="cart" element={<Cart />} />
     </Route>
